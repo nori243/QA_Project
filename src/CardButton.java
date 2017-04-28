23af382c4a0bb8c17ddbb669fb13fbaf9f84538b
nori243 <bcat243@gmail.com>
2017-04-28 20:25:55 +0800

    更改 - card圖片listener

--- src/CardButton.java
+++ src/CardButton.java
@@ -1,6 +1,8 @@
 import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
@@ -20,22 +22,47 @@ public class CardButton extends JButton
 		
 		this.setBorder(BorderFactory.createEmptyBorder());
 		this.setContentAreaFilled(false);
+		this.addMouseListener(new MouseListener()
+		{
+
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void mouseEntered(MouseEvent arg0) {
+				// TODO Auto-generated method stub
+				
+				//	isPick = false;
+				CardButton.this.setBorder(BorderFactory.createLineBorder(Color.RED, 1));
+				
+			}
+
+			@Override
+			public void mouseExited(MouseEvent arg0) {
+				// TODO Auto-generated method stub
+				CardButton.this.setBorder(BorderFactory.createEmptyBorder());
+			}
+
+			@Override
+			public void mousePressed(MouseEvent arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void mouseReleased(MouseEvent arg0) {
+				// TODO Auto-generated method stub
+				
+			}});
 		this.addActionListener(new ActionListener()
 		{
 			@Override
 			public void actionPerformed(ActionEvent arg0) 
 			{
-				if(isPick)
-				{
-					CardButton.this.setBorder(BorderFactory.createEmptyBorder());
-					isPick = false;
-				}
-					
-				else
-				{
-					CardButton.this.setBorder(BorderFactory.createLineBorder(Color.RED, 1));
-					isPick = true;
-				}
+				
 					
 			}
 		});
