77eec158e6f72ee954a77b98ad4a81e17a08c3fd
nori243 <bcat243@gmail.com>
2017-05-21 16:48:28 +0800

    新增 - 起始Main

--- src/MainTest.java
+++ src/MainTest.java
@@ -1,24 +1,13 @@
 
 public class MainTest 
 {
-
 	public static void main(String[] args) 
 	{		
-		Pile pile = new Pile();
+		StartFrame start = new StartFrame();
 		
-		Player player = new Player("May",pile);
-		
-		System.out.println(player.showCard());
-		System.out.println(player.checkJoker());
-		
-		player.removeCard(new Card(0,0));
-		System.out.println(player.checkJoker());
-		System.out.println(player.showCard());
-		
-		player.init("may", new Pile());
-		System.out.println(player.showCard());
-		
-		
+		start.setVisible(true);			
 	}
+	
+
 
 }
