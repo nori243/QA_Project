0f9583de69029fbc22bc232332d639db014488de
nori243 <bcat243@gmail.com>
2017-05-29 15:56:25 +0800

    新增 - 翻面

--- src/CardButton.java
+++ src/CardButton.java
@@ -3,6 +3,8 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.util.Observable;
+import java.util.Observer;
 
 import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
@@ -14,50 +16,37 @@ public class CardButton extends JButton
 	private int playerIndex = -1;
 	private int cardIndex ;
 	private boolean isPick = false;
+	private boolean isBack = false;
+	private ImageIcon img;
+	private ImageIcon back;
 	
 	public CardButton(ImageIcon img,int cardIndex)
 	{
 		super(img);
+		this.img = img;
 		this.cardIndex = cardIndex;
+		back = new ImageIcon(System.getProperty("user.dir")+"//cardPic//back.gif");
+		
 		
 		this.setBorder(BorderFactory.createEmptyBorder());
 		this.setContentAreaFilled(false);
-		this.addMouseListener(new MouseListener()
-		{
-
-			@Override
-			public void mouseClicked(MouseEvent arg0) {
-				// TODO Auto-generated method stub
-				
-			}
-
-			@Override
-			public void mouseEntered(MouseEvent arg0) {
-				// TODO Auto-generated method stub
-				
-				//	isPick = false;
-				CardButton.this.setBorder(BorderFactory.createLineBorder(Color.RED, 1));
-				
-			}
-
-			@Override
-			public void mouseExited(MouseEvent arg0) {
-				// TODO Auto-generated method stub
-				CardButton.this.setBorder(BorderFactory.createEmptyBorder());
-			}
-
-			@Override
-			public void mousePressed(MouseEvent arg0) {
-				// TODO Auto-generated method stub
-				
-			}
-
-			@Override
-			public void mouseReleased(MouseEvent arg0) {
-				// TODO Auto-generated method stub
-				
-			}});
 		
+		
+		
+	}
+	
+	public void turnBack(boolean turn)
+	{
+		isBack = turn;
+		
+		if(turn)
+		{
+			this.setIcon(back);
+		}
+		else
+		{
+			this.setIcon(img);
+		}
 		
 	}
 	
@@ -75,6 +64,12 @@ public class CardButton extends JButton
 	{
 		return this.playerIndex;
 	}
+
+	public void setFrame()
+	{
+		
+	}
+	
 
 //animate : http://www.cnblogs.com/xiaowenji/archive/2011/02/04/1949165.html
 }
